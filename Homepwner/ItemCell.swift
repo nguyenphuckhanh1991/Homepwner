0e262579f9055bc1bd818931ba94b3032bd75597
nguyen.phuc.khanh <nguyen.phuc.khanh@framgias-mac-mini.local>
2017-11-01 16:00:00 +0700

    Using ItemCell

---
+++ Homepwner/ItemCell.swift
@@ -0,0 +1,27 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by nguyen.phuc.khanh on 11/1/17.
+//  Copyright © 2017 nguyen.phuc.khanh. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var serialNumberLabel: UILabel!
+    @IBOutlet weak var valueLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
