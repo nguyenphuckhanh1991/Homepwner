2dde8bb35a016bfcef16ec481c86a518ed600761
nguyen.phuc.khanh <nguyen.phuc.khanh@framgias-mac-mini.local>
2017-11-01 15:27:19 +0700

    Deleting Rows

--- Homepwner/ItemStore.swift
+++ Homepwner/ItemStore.swift
@@ -25,4 +25,10 @@ class ItemStore {
         
         return newItem
     }
+    
+    func removeItem(_ item: Item) {
+        if let index = allItems.index(of: item) {
+            allItems.remove(at: index)
+        }
+    }
 }
--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -86,17 +86,21 @@ class ItemsViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        // If the table view is asking to commit a delete command...
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
+            let item = itemStore.allItems[indexPath.row]
+            //Remove the item from the store
+            itemStore.removeItem(item)
+            // Also remove that row from the tableview with an animation
+            tableView.deleteRows(at: [indexPath], with: .automatic)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
